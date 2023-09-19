c7b66038e91cd7d880558d1eb96fdb20fb53161c
decant09 <>
2023-09-19 11:52:16 +0000

    Add post detail

--- blog/views.py
+++ blog/views.py
@@ -22,7 +22,7 @@ class PostDetail(View):
 
         return render(
             request,
-            "post_detil.html",
+            "post_detail.html",
             {
                 "post": post,
                 "comments": comments,
